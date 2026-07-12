32f41d108aa0424b07e6a2069d83d5a17db4e46d
paullarkiin <paularkin@hotmail.com>
2026-07-12 23:12:42 +0200

    Removing additional padding on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,11 +4,11 @@ import { LiveClock } from "@/components/Clock";
 export function Footer() {
   return (
     <div className="flex flex-col w-full items-center bg-background font-sans mb-24 ">
-      <footer className="flex w-full max-w-3xl justify-between px-16 pb-16 pt-4 text-[14px] text-text-muted font-sans">
-          <span>{siteConfig.copyright}</span>
-          <span className="flex items-center gap-2">
-            {siteConfig.status} <LiveClock />
-          </span>
+      <footer className="flex w-full max-w-3xl justify-between px-16 pt-4 text-[14px] text-text-muted font-sans">
+        <span>{siteConfig.copyright}</span>
+        <span className="flex items-center gap-2">
+          {siteConfig.status} <LiveClock />
+        </span>
       </footer>
     </div>
   );
